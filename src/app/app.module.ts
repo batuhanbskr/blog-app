a01ae4234e2f1b2e2837456b173431d0397b3a05
batuhan <batuhanbskr@gmail.com>
2023-08-28 10:55:36 +0300

    Blog App Created
    angular blog app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { InfoComponent } from './info/info.component';
+import { ContactComponent } from './contact/contact.component';
+import { PostsComponent } from './posts/posts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InfoComponent,
+    ContactComponent,
+    PostsComponent
   ],
   imports: [
     BrowserModule
